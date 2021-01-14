934d97324f75e9fe6a08ba5e2318501cc391f165
Max Smoot <smootmw@appstate.edu>
2021-01-13 20:05:49 -0500

    fixed cors policy when deployed

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(bodyparser.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: process.env.NODE_ENV == "production" ? "http://localhost:8080": "https://www.trails.maxwsmoot.com",
     optionsSuccessStatus: 200,
     credentials: true,
   })
