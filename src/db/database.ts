4c0bf0f7b9acd0bca89f9520da46a83cddf75cf6
Max Smoot <smootmw@appstate.edu>
2021-02-01 23:39:00 -0500

    fixes

--- src/db/database.ts
+++ src/db/database.ts
@@ -12,7 +12,7 @@ export async function createPool() {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "process.env.DB_NAME",
+    database: process.env.DB_NAME,
     multipleStatements: true,
   });
 }
